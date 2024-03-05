1a07d265a472cad8d48094d3f1b8a35587d9d368
guanhua yao <ygh2334@awzg.office.gy>
2024-03-05 22:50:53 +0800

    fix:临时提交

--- GameEngine/src/Biscuit.h
+++ GameEngine/src/Biscuit.h
@@ -3,7 +3,9 @@
 //For use by Biscuit applications
 #include <stdio.h>
 #include "Biscuit/Application.h"
+//#include "Biscuit/Log.h"
 
 //---Entry Point-----------------------
 #include "Biscuit/EntryPoint.h"
 //-------------------------------------
+
--- GameEngine/src/Biscuit/EntryPoint.h
+++ GameEngine/src/Biscuit/EntryPoint.h
@@ -6,6 +6,8 @@ extern Biscuit::Application* Biscuit::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	//Biscuit::Log::Init();
+	Biscuit::Log::Init();
 	printf("Biscuit Engine\n");
 	auto app = Biscuit::CreateApplication();
 	app->Run();
